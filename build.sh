4c763bd61159aac7187eb6cad9283b9e07ebb887
amme00 <chaudbobop@gmail.com>
2025-06-21 17:16:21 +0100

    superuser

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "creating superuser"
+
+pyhon manage.py createsuperuser --noinput || true
